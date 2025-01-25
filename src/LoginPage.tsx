2ab274b7003f86dcedd2bcf8ada4f30fc9d388dc
Beksultan A <babdikaarov@gmail.com>
2025-01-25 19:54:26 +0600

    clean up login

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -1,57 +1,56 @@
 // LoginPage.js
 
-import { Login, LoginForm, ToggleThemeButton} from "react-admin";
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup, sendPasswordResetEmail  } from "firebase/auth";
-import { config } from "./providers/providers";
-import { CardContent, Dialog, DialogActions, DialogContent, DialogTitle, Button, TextField, Typography,Box } from '@mui/material';
+import { Login, LoginForm } from "react-admin";
+import { getAuth, GoogleAuthProvider, signInWithPopup, sendPasswordResetEmail } from "firebase/auth";
+import {
+   CardContent,
+   Dialog,
+   DialogActions,
+   DialogContent,
+   DialogTitle,
+   Button,
+   TextField,
+   Typography,
+   Box,
+} from "@mui/material";
 import { useState } from "react";
-import googleIcon from '../public/google-icon.svg'
-
-import { getAuth, GoogleAuthProvider, sendPasswordResetEmail, signInWithPopup } from "firebase/auth";
+import googleIcon from "../public/google-icon.svg";
 import { app } from "./providers/providers";
-import ForgotPassword from "./ForgotPassword";
-
-// Initialize Firebase
 
 const auth = getAuth(app);
 auth.languageCode = "en"; // Set language (optional)
 
 const CustomLoginForm = (props: JSX.IntrinsicAttributes) => {
    const [open, setOpen] = useState(false);
-   const [email, setEmail] = useState('');
+   const [email, setEmail] = useState("");
    const [isSubmitting, setIsSubmitting] = useState(false);
-   const [message, setMessage] = useState('');
-
-   const handleClickOpen = () => {
-     setOpen(true);
-   };
+   const [message, setMessage] = useState("");
 
    const handleClose = () => {
-     setOpen(false);
-     setMessage(''); // Clear message on close
+      setOpen(false);
+      setMessage(""); // Clear message on close
    };
 
    const handleEmailChange = (event) => {
-     setEmail(event.target.value);
+      setEmail(event.target.value);
    };
 
    const handleSendReset = () => {
       setIsSubmitting(true);
       sendPasswordResetEmail(auth, email)
-        .then(() => {
-          setIsSubmitting(false);
-          setMessage('A password reset email has been sent to your email address.');
-        })
-        .catch((error) => {
-          setIsSubmitting(false);
-          setMessage(`Error: ${error.message}`);
-        });
+         .then(() => {
+            setIsSubmitting(false);
+            setMessage("A password reset email has been sent to your email address.");
+         })
+         .catch((error) => {
+            setIsSubmitting(false);
+            setMessage(`Error: ${error.message}`);
+         });
    };
 
    const handleReset = () => {
       setOpen(true); // Open the dialog
-      setMessage(''); // Clear any previous message
+      setMessage(""); // Clear any previous message
    };
 
    const handleGoogleLogin = async () => {
@@ -74,65 +73,56 @@ const CustomLoginForm = (props: JSX.IntrinsicAttributes) => {
    };
 
    return (
-
-      <div style={{display: 'flex', justifyContent: 'center',flexDirection: 'column', alignItems: 'center'}}>
-
-      <LoginForm {...props} />
-      <Box sx={{ width: '100%', maxWidth: 400 }}>
-      <CardContent>
-         <Button 
-            onClick={handleGoogleLogin} 
-            fullWidth 
-            variant="outlined"
-            startIcon={
-               <img 
-                  src={googleIcon} 
-                  alt="Google Logo" 
-                  style={{ width: '28px', height: '28px'}} 
-               />
-            } // Inherit original colors
-            style={{ color: '#92ccfa' }} 
-/*             sx={{
+      <div style={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center" }}>
+         <LoginForm {...props} />
+         <Box sx={{ width: "100%", maxWidth: 400 }}>
+            <CardContent>
+               <Button
+                  onClick={handleGoogleLogin}
+                  fullWidth
+                  variant="outlined"
+                  startIcon={<img src={googleIcon} alt="Google Logo" style={{ width: "28px", height: "28px" }} />} // Inherit original colors
+                  style={{ color: "#92ccfa" }}
+                  /*             sx={{
                backgroundColor:  '#fff'
                
             }} */
-         >
-            Sign in with Google
-         </Button>
-      </CardContent>
+               >
+                  Sign in with Google
+               </Button>
+            </CardContent>
 
-{/*       <ToggleThemeButton/> */}
-         
-      <Button
-         style={{
-            textDecoration: 'underline',
-            textDecorationThickness: '1.5px',
-            textDecorationColor: 'gray', // Default underline color
-            color: "gray", // Default text color
-            transition: 'all 0.3s ease', // Smooth transition for color change and opacity
-         }} 
-         onClick={handleReset}
-         fullWidth
-         onMouseEnter={(e) => {
-            e.target.style.color = '#92ccfa'; 
-            e.target.style.textDecorationColor = '#92ccfa'; 
-            e.target.style.opacity = 0.8; // Add opacity change on hover
-         }}
-         onMouseLeave={(e) => {
-            e.target.style.color = 'gray';
-            e.target.style.textDecorationColor = 'gray';
-            e.target.style.opacity = 1; // Revert opacity to default
-         }}
-      >
-         Forgot Password
-      </Button>
-      
+            {/*       <ToggleThemeButton/> */}
 
-         <Dialog open={open} onClose={handleClose}> 
-            <DialogTitle>Send Password Reset</DialogTitle>
-            <DialogContent>
-               <Typography variant="body1">A password reset will be sent to the following email:</Typography>
-               <TextField
+            <Button
+               style={{
+                  textDecoration: "underline",
+                  textDecorationThickness: "1.5px",
+                  textDecorationColor: "gray", // Default underline color
+                  color: "gray", // Default text color
+                  transition: "all 0.3s ease", // Smooth transition for color change and opacity
+               }}
+               onClick={handleReset}
+               fullWidth
+               onMouseEnter={(e) => {
+                  e.target.style.color = "#92ccfa";
+                  e.target.style.textDecorationColor = "#92ccfa";
+                  e.target.style.opacity = 0.8; // Add opacity change on hover
+               }}
+               onMouseLeave={(e) => {
+                  e.target.style.color = "gray";
+                  e.target.style.textDecorationColor = "gray";
+                  e.target.style.opacity = 1; // Revert opacity to default
+               }}
+            >
+               Forgot Password
+            </Button>
+
+            <Dialog open={open} onClose={handleClose}>
+               <DialogTitle>Send Password Reset</DialogTitle>
+               <DialogContent>
+                  <Typography variant="body1">A password reset will be sent to the following email:</Typography>
+                  <TextField
                      label="Email Address"
                      type="email"
                      fullWidth
@@ -140,26 +130,26 @@ const CustomLoginForm = (props: JSX.IntrinsicAttributes) => {
                      onChange={handleEmailChange}
                      margin="normal"
                      autoFocus
-               />
-               {message && <Typography variant="body2" color="success.main" mt={2}>{message}</Typography>}
-            </DialogContent>
-            <DialogActions>
-               <Button onClick={handleClose} color="secondary" disabled={isSubmitting}>Cancel</Button>
-               <Button
-                     onClick={handleSendReset}
-                     color="primary"
-                     disabled={isSubmitting || !email}
-               >
-                     {isSubmitting ? 'Sending...' : 'Send Email'}
-               </Button>
-            </DialogActions>
-         </Dialog>
-      </Box>
+                  />
+                  {message && (
+                     <Typography variant="body2" color="success.main" mt={2}>
+                        {message}
+                     </Typography>
+                  )}
+               </DialogContent>
+               <DialogActions>
+                  <Button onClick={handleClose} color="secondary" disabled={isSubmitting}>
+                     Cancel
+                  </Button>
+                  <Button onClick={handleSendReset} color="primary" disabled={isSubmitting || !email}>
+                     {isSubmitting ? "Sending..." : "Send Email"}
+                  </Button>
+               </DialogActions>
+            </Dialog>
+         </Box>
       </div>
-
    );
 };
-
 
 const CustomLoginPage = (props: JSX.IntrinsicAttributes) => (
    <Login {...props}>
